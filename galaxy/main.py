ceab9e1de6f929ec2cffd6d504eb03bbd89db87b
Aouzou <6163852@crosemont.qc.ca>
2023-12-30 01:43:30 -0500

    déplacement gauche/droite avec clique

--- galaxy/main.py
+++ galaxy/main.py
@@ -22,7 +22,8 @@ class MainWidget(Widget):
     SPEED = 2
     current_offset_y = 0
 
-    SPEED_X = 3
+    SPEED_X = 12
+    current_speed_x = 0
     current_offset_x = 0
     def __init__(self, **kwargs):
         super(MainWidget, self).__init__(**kwargs)
@@ -116,6 +117,17 @@ class MainWidget(Widget):
         tr_y = self.perspective_point_y - factor_y*self.perspective_point_y
         return int(tr_x), int(tr_y)
 
+    def on_touch_down(self, touch):
+        if touch.x < self.width/2:
+            print("<-")
+            self.current_speed_x = self.SPEED_X
+        else:
+            print("->")
+            self.current_speed_x = -self.SPEED_X
+
+
+    def on_touch_up(self, touch):
+        print("UP")
 
     def update(self, dt):
         #print("dt : " + str(dt*60))
@@ -128,7 +140,7 @@ class MainWidget(Widget):
         if self.current_offset_y >= spacing_y:
             self.current_offset_y-=spacing_y
 
-        self.current_offset_x += self.SPEED_X * time_factor
+        self.current_offset_x += self.current_speed_x * time_factor
 
 
 
